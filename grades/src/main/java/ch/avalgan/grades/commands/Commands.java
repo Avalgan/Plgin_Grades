7a8869229a90918afa69f38ce78a525af11d311c
3rty1ne <mcserveur31@gmail.com>
2020-04-08 22:22:30 +0200

    gestion des grades

--- grades/src/main/java/ch/avalgan/grades/commands/Commands.java
+++ grades/src/main/java/ch/avalgan/grades/commands/Commands.java
@@ -1,5 +1,6 @@
 package ch.avalgan.grades.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,14 +12,80 @@ public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player player = (Player) sender;
 		if (label.equalsIgnoreCase("gd")) {
-			if (args.length !=2) {
+			if (args.length != 2) {
 				sender.sendMessage("Mauvaise utilisation,/gd <Grade> <Player>");
-			} 
-		if (sender instanceof Player) {
-			if (player.hasPermission("gd.use")) {
-				
 			}
-		}
+			if (sender instanceof Player) {
+				if (player.hasPermission("gd.use")) {
+					Player target = Bukkit.getPlayer(args[1]);
+					if (target != null) {
+						if (args[0].equalsIgnoreCase("militaire")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+									"manuadd " + target.getName() + " militaire");
+						}
+						if (args[0].equalsIgnoreCase("soldat")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+									"manuadd " + target.getName() + " soldat");
+						}
+						if (args[0].equalsIgnoreCase("caporal")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+									"manuadd " + target.getName() + " caporal");
+						}
+						if (args[0].equalsIgnoreCase("major")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " major");
+						}
+						if (args[0].equalsIgnoreCase("colonel")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+									"manuadd " + target.getName() + " colonel");
+						}
+						if (args[0].equalsIgnoreCase("general")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+									"manuadd " + target.getName() + " general");
+						}
+						if (args[0].equalsIgnoreCase("guide")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " guide");
+						}
+						if (args[0].equalsIgnoreCase("streamer")) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+									"manuadd " + target.getName() + " streamer");
+						}
+					} else {
+						player.sendMessage("Ce joueur n'est pas en ligne! || Ou n'existe pas!");
+					}
+				} else {
+					player.sendMessage("Vous n'avez pas la permission!");
+				}
+			} else {
+				Player target = Bukkit.getPlayer(args[1]);
+				if (target != null) {
+					if (args[0].equalsIgnoreCase("militaire")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " militaire");
+					}
+					if (args[0].equalsIgnoreCase("soldat")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " soldat");
+					}
+					if (args[0].equalsIgnoreCase("caporal")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " caporal");
+					}
+					if (args[0].equalsIgnoreCase("major")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " major");
+					}
+					if (args[0].equalsIgnoreCase("colonel")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " colonel");
+					}
+					if (args[0].equalsIgnoreCase("general")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " general");
+					}
+					if (args[0].equalsIgnoreCase("guide")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " guide");
+					}
+					if (args[0].equalsIgnoreCase("streamer")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " streamer");
+					}
+				} else {
+					sender.sendMessage("Ce joueur n'est pas en ligne! || Ou n'existe pas!");
+				}
+			}
 		}
 		return false;
 	}
