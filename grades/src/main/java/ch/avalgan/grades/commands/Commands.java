47e1526b898f8a1aaf69498514ea439cc1b306a5
Beyrand Alexis <jn.beyrand@gmail.com>
2020-04-09 11:38:46 +0200

    Message Rankup/derank

--- grades/src/main/java/ch/avalgan/grades/commands/Commands.java
+++ grades/src/main/java/ch/avalgan/grades/commands/Commands.java
@@ -31,68 +31,182 @@ public class Commands implements CommandExecutor {
 							}
 							sql.deleteReport(target);
 							sql.createGrade(target, "militaire");
+							return true;
 						}
 						if (args[0].equalsIgnoreCase("soldat")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
 									"manuadd " + target.getName() + " soldat");
+							if (sql.getGrades(target).equalsIgnoreCase("millitaire")) {
+								target.sendMessage("Vous avez été promu!");
+							} else {
+								target.sendMessage("Vous avez été unrank !");
+							}
+							sql.deleteReport(target);
+							sql.createGrade(target, "soldat");
+							return true;
 						}
 						if (args[0].equalsIgnoreCase("caporal")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
 									"manuadd " + target.getName() + " caporal");
+							if (sql.getGrades(target).equalsIgnoreCase("militaire")
+									|| sql.getGrades(target).equalsIgnoreCase("soldat")) {
+								target.sendMessage("Vous avez été promu !");
+							} else {
+								target.sendMessage("Vous avez été unrank !");
+							}
+							sql.deleteReport(target);
+							sql.createGrade(target, "caporal");
+							return true;
 						}
 						if (args[0].equalsIgnoreCase("major")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " major");
+							if (sql.getGrades(target).equalsIgnoreCase("colonel")
+									|| sql.getGrades(target).equalsIgnoreCase("general")) {
+								target.sendMessage("Vous avez été unrank !");
+							} else {
+								target.sendMessage("Vous avez été promu !");
+							}
+							sql.deleteReport(target);
+							sql.createGrade(target, "major");
+							return true;
 						}
 						if (args[0].equalsIgnoreCase("colonel")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
 									"manuadd " + target.getName() + " colonel");
+							if (sql.getGrades(target).equalsIgnoreCase("general")) {
+								target.sendMessage("vous avez été unrank !");
+							} else {
+								target.sendMessage("Vous avez été promu !");
+							}
+							sql.deleteReport(target);
+							sql.createGrade(target, "colonel");
+							return true;
 						}
 						if (args[0].equalsIgnoreCase("general")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
 									"manuadd " + target.getName() + " general");
+							target.sendMessage("Vous avez été promu général !");
+							sql.deleteReport(target);
+							sql.createGrade(target, "general");
+							return true;
 						}
+
 						if (args[0].equalsIgnoreCase("guide")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " guide");
+							sql.deleteReport(target);
+							sql.createGrade(target, "guide");
+							return true;
 						}
 						if (args[0].equalsIgnoreCase("streamer")) {
 							Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
 									"manuadd " + target.getName() + " streamer");
+							target.sendMessage(
+									"Vous avez atteint l'objectif pour etre reconnu comme streamer/youtuber sur le serveur !");
+							sql.deleteReport(target);
+							sql.createGrade(target, "streamer");
+							return true;
 						}
+						player.sendMessage("Ce grade n'est pas valide");
+						return false;
 					} else {
 						player.sendMessage("Ce joueur n'est pas en ligne! || Ou n'existe pas!");
+						return false;
 					}
 				} else {
 					player.sendMessage("Vous n'avez pas la permission!");
+					return false;
 				}
 			} else {
 				Player target = Bukkit.getPlayer(args[1]);
 				if (target != null) {
 					if (args[0].equalsIgnoreCase("militaire")) {
-						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " militaire");
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+								"manuadd " + target.getName() + " militaire");
+						if (!sql.getGrades(target).equalsIgnoreCase("militaire")) {
+							target.sendMessage("Vous avez été unranked!");
+						}
+						sql.deleteReport(target);
+						sql.createGrade(target, "militaire");
+						return true;
 					}
 					if (args[0].equalsIgnoreCase("soldat")) {
-						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " soldat");
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+								"manuadd " + target.getName() + " soldat");
+						if (sql.getGrades(target).equalsIgnoreCase("millitaire")) {
+							target.sendMessage("Vous avez été promu!");
+						} else {
+							target.sendMessage("Vous avez été unrank !");
+						}
+						sql.deleteReport(target);
+						sql.createGrade(target, "soldat");
+						return true;
 					}
 					if (args[0].equalsIgnoreCase("caporal")) {
-						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " caporal");
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+								"manuadd " + target.getName() + " caporal");
+						if (sql.getGrades(target).equalsIgnoreCase("militaire")
+								|| sql.getGrades(target).equalsIgnoreCase("soldat")) {
+							target.sendMessage("Vous avez été promu !");
+						} else {
+							target.sendMessage("Vous avez été unrank !");
+						}
+						sql.deleteReport(target);
+						sql.createGrade(target, "caporal");
+						return true;
 					}
 					if (args[0].equalsIgnoreCase("major")) {
 						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " major");
+						if (sql.getGrades(target).equalsIgnoreCase("colonel")
+								|| sql.getGrades(target).equalsIgnoreCase("general")) {
+							target.sendMessage("Vous avez été unrank !");
+						} else {
+							target.sendMessage("Vous avez été promu !");
+						}
+						sql.deleteReport(target);
+						sql.createGrade(target, "major");
+						return true;
 					}
 					if (args[0].equalsIgnoreCase("colonel")) {
-						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " colonel");
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+								"manuadd " + target.getName() + " colonel");
+						if (sql.getGrades(target).equalsIgnoreCase("general")) {
+							target.sendMessage("vous avez été unrank !");
+						} else {
+							target.sendMessage("Vous avez été promu !");
+						}
+						sql.deleteReport(target);
+						sql.createGrade(target, "colonel");
+						return true;
 					}
 					if (args[0].equalsIgnoreCase("general")) {
-						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " general");
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+								"manuadd " + target.getName() + " general");
+						target.sendMessage("Vous avez été promu général !");
+						sql.deleteReport(target);
+						sql.createGrade(target, "general");
+						return true;
 					}
+
 					if (args[0].equalsIgnoreCase("guide")) {
 						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " guide");
+						sql.deleteReport(target);
+						sql.createGrade(target, "guide");
+						return true;
 					}
 					if (args[0].equalsIgnoreCase("streamer")) {
-						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "manuadd " + target.getName() + " streamer");
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+								"manuadd " + target.getName() + " streamer");
+						target.sendMessage(
+								"Vous avez atteint l'objectif pour etre reconnu comme streamer/youtuber sur le serveur !");
+						sql.deleteReport(target);
+						sql.createGrade(target, "streamer");
+						return true;
 					}
+					sender.sendMessage("Ce grade n'est pas valide !");
+					return false;
 				} else {
-					sender.sendMessage("Ce joueur n'est pas en ligne! || Ou n'existe pas!");
+					player.sendMessage("Ce joueur n'est pas en ligne! || Ou n'existe pas!");
+					return false;
 				}
 			}
 		}
